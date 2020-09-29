fb14a7e7c7804283ad6f923902c7108cad7bf527
konnik <niklas.konstenius@gmail.com>
2020-09-29 21:22:25 +0200

    Elm UI

---
+++ karta.js
@@ -0,0 +1,65 @@
+import L from 'leaflet';
+import testeboan from "./forsar/testeboan";
+import gavlean from "./forsar/gavlean";
+
+export default function() {
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    });
+
+
+    const gavle = [60.719459, 17.054273];
+    const sverige = [63.031926, 15.451756]
+
+    var map = L.map("map").setView(gavle, 10);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoia29ubmlrIiwiYSI6ImNrZm13azN2ajFmMWozMHFoZ3F3czFneW0ifQ.6vvhWXlFXhso6ip9QPMyqA'
+    })//.addTo(map);
+
+    const topowebbUrl = "https://api.lantmateriet.se/open/topowebb-ccby/v1/wmts/token/{accessToken}/1.0.0/{layer}/default/3857/{z}/{y}/{x}.png"
+    L.tileLayer(topowebbUrl, {
+        accessToken: "b8913fdb-6555-3591-9cbb-6f1456ee1490",
+        layer: "topowebb_nedtonad",
+    maxZoom: 15,
+    minZoom: 0,
+    continuousWorld: true,
+    attribution: '&copy; <a href="https://www.lantmateriet.se/en/">Lantmäteriet</a> Topografisk Webbkarta Visning, CCB',
+    }).addTo(map);
+
+
+    L.geoJSON([testeboan, gavlean ], {
+        style: {
+            color: "#0000ff",
+            weight: 5
+        },
+        filter: (feature, layer) => {
+            console.log("filter", feature.properties, layer)
+            return feature.properties.typ == "fors";
+        },
+        onEachFeature:  (feature, layer) => {
+            console.log("onEachFeature", feature.properties)
+            if (feature.properties.typ === "fors") {
+
+                layer.bindTooltip(feature.properties.namn + " (" + feature.properties.vattendrag + ")", {
+                    sticky : true
+                });
+                // layer.on('mouseover', function (e) {
+                //     this.openPopup();
+                // });
+                // layer.on('mouseout', function (e) {
+                //     this.closePopup();
+                // });
+            }
+        }
+    }).addTo(map);
+}
--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,99 @@
 module Main exposing (main)
 
-import Html exposing (Html, h1, text)
+import Element exposing (Element, alignBottom, alignLeft, alignRight, centerX, centerY, column, el, fill, height, inFront, layout, padding, paragraph, px, rgb255, row, shrink, spacing, text, width)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Html exposing (Attribute, Html, div, h1)
+import Html.Attributes as HtmlAttr
+
+
+white : Element.Color
+white =
+    rgb255 255 255 255
+
+
+blue : Element.Color
+blue =
+    rgb255 45 124 172
+
+
+lightBlue : Element.Color
+lightBlue =
+    rgb255 172 219 255
 
 
 main : Html msg
 main =
-    h1 [] [ text "Forsguiden PoC!" ]
+    layout [ width fill, height fill ] <|
+        column [ width fill, height fill ]
+            [ header
+            , mainContent
+            , footer
+            ]
+
+
+header : Element msg
+header =
+    el
+        [ width fill
+        , height (px 50)
+        , Border.widthEach { top = 0, bottom = 5, left = 0, right = 0 }
+        , Border.color blue
+        ]
+        (el [ centerX, centerY ] (Element.text "Forsguiden PoC"))
+
+
+footer : Element msg
+footer =
+    el
+        [ alignBottom
+        , width fill
+        , height (px 20)
+        , Border.widthEach { top = 3, bottom = 0, left = 0, right = 0 }
+        , Border.color blue
+        ]
+        (el
+            [ Font.size 10
+            , centerX
+            , centerY
+            ]
+            (text "© Elm Lovers")
+        )
+
+
+mainContent : Element msg
+mainContent =
+    el
+        [ width fill
+        , height fill
+        , Background.color white
+        ]
+        mapView
+
+
+mapView : Element msg
+mapView =
+    el
+        [ width fill
+        , height fill
+        ]
+        (Element.html mapHtml)
+
+
+mapHtml : Html msg
+mapHtml =
+    Html.div
+        [ HtmlAttr.style "width" "100%"
+        , HtmlAttr.style "height" "100%"
+        , HtmlAttr.style "z-index" "0"
+        ]
+        [ Html.div
+            [ HtmlAttr.id "map"
+            , HtmlAttr.style "width" "100%"
+            , HtmlAttr.style "height" "100%"
+            , HtmlAttr.style "position" "relative"
+            , HtmlAttr.style "z-index" "0"
+            ]
+            []
+        ]
