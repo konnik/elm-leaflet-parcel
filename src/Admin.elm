2be9de14dacd2d5a4168f96134869e172fc87a2a
Niklas Konstenius <niklas.konstenius@lm.se>
2021-01-22 16:35:26 +0100

    visa data

--- src/Admin.elm
+++ src/Admin.elm
@@ -24,12 +24,26 @@ type alias Model =
     { key : Nav.Key
     , url : Url.Url
     , forsar : List Fors
+    , vattendrag : List Vattendrag
+    , lan : List Lan
     }
+
+
+type alias Fors =
+    { id : Int, namn : String }
+
+
+type alias Vattendrag =
+    { id : Int, namn : String }
+
+
+type alias Lan =
+    { id : Int, namn : String }
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-    ( Model key url [], hamtaForsar )
+    ( Model key url [] [] [], Cmd.batch [ hamtaForsar, hamtaVattendrag, hamtaLan ] )
 
 
 hamtaForsar : Cmd Msg
@@ -47,12 +61,40 @@ forsDecoder =
         (D.field "namn" D.string)
 
 
-type alias Fors =
-    { id : Int, namn : String }
+hamtaVattendrag : Cmd Msg
+hamtaVattendrag =
+    Http.get
+        { url = "https://forsguiden-api.herokuapp.com/vattendrag"
+        , expect = Http.expectJson GotVattendrag (D.field "vattendrag" (D.list vattendragDecoder))
+        }
+
+
+vattendragDecoder : D.Decoder Vattendrag
+vattendragDecoder =
+    D.map2 Vattendrag
+        (D.field "id" D.int)
+        (D.field "namn" D.string)
+
+
+hamtaLan : Cmd Msg
+hamtaLan =
+    Http.get
+        { url = "https://forsguiden-api.herokuapp.com/lan"
+        , expect = Http.expectJson GotLan (D.field "lan" (D.list lanDecoder))
+        }
+
+
+lanDecoder : D.Decoder Lan
+lanDecoder =
+    D.map2 Lan
+        (D.field "id" D.int)
+        (D.field "namn" D.string)
 
 
 type Msg
     = GotForsar (Result Http.Error (List Fors))
+    | GotVattendrag (Result Http.Error (List Vattendrag))
+    | GotLan (Result Http.Error (List Lan))
     | UrlRequested Browser.UrlRequest
     | UrlChanged Url.Url
 
@@ -64,6 +106,18 @@ update msg model =
             ( { model | forsar = forsar }, Cmd.none )
 
         GotForsar (Err err) ->
+            ( model, Cmd.none )
+
+        GotVattendrag (Ok vattendrag) ->
+            ( { model | vattendrag = vattendrag }, Cmd.none )
+
+        GotVattendrag (Err err) ->
+            ( model, Cmd.none )
+
+        GotLan (Ok lan) ->
+            ( { model | lan = lan }, Cmd.none )
+
+        GotLan (Err err) ->
             ( model, Cmd.none )
 
         UrlRequested urlRequest ->
@@ -90,6 +144,42 @@ view model =
     { title = "Application Title"
     , body =
         [ div []
-            [ text <| "Antal forsar: " ++ String.fromInt (List.length model.forsar) ]
+            [ p []
+                [ text <| "Forsar (" ++ String.fromInt (List.length model.forsar) ++ ")"
+                , div [] [ forsarView model.forsar ]
+                ]
+            , p []
+                [ text <| "Vattendrag (" ++ String.fromInt (List.length model.vattendrag) ++ ")"
+                , div [] [ vattendragView model.vattendrag ]
+                ]
+            , p []
+                [ text <| "Län (" ++ String.fromInt (List.length model.lan) ++ ")"
+                , div [] [ lanView model.lan ]
+                ]
+            ]
         ]
     }
+
+
+forsarView : List Fors -> Html msg
+forsarView forsar =
+    forsar
+        |> List.map .namn
+        |> String.join ", "
+        |> text
+
+
+vattendragView : List Vattendrag -> Html msg
+vattendragView vattendrag =
+    vattendrag
+        |> List.map .namn
+        |> String.join ", "
+        |> text
+
+
+lanView : List Lan -> Html msg
+lanView lan =
+    lan
+        |> List.map .namn
+        |> String.join ", "
+        |> text
